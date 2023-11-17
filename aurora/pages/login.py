46a81a224173c9331e6988ad61127f66f07423f0
SE0NGH0 <ericsungho@naver.com>
2023-11-17 09:33:17 +0900

    수정

---
+++ aurora/pages/login.py
@@ -0,0 +1,89 @@
+"""Login page. Uses auth_layout to render UI shared with the sign up page."""
+import reflex as rx
+
+from aurora.state.auth import AuthState
+
+
+def login():
+    return rx.container(
+        rx.container(height='200px'), 
+        rx.vstack(           
+            rx.heading(
+                rx.text(
+                    "Aurora!",
+                    style={
+                        "fontSize": "40px",
+                        "fontWeight": "bolder",
+                        "letterSpacing": "5px",
+                        "fontFamily": "Open Sans,Sans-serif",
+                        "background": "-webkit-linear-gradient(-45deg, #e04a3f, #4e8be6)",
+                        "-webkit-background-clip": "text",
+                        "color": "transparent",
+                    },
+                    center_content=True,
+                ),
+                display="flex",
+                flex_direction="column",
+                align_items="center",
+                text_align="center",
+            ),
+            rx.text(
+                "Create a picture with your story!",
+                color="gray.500",
+                font_weight="medium",
+            ),
+            rx.container(
+                rx.container(height='30px'),
+                rx.image(
+                    src="C:/Users/a/Desktop/vscodeGithub/aurora/assets/favicon.ico",
+                    alt="star",
+                    style={"width": "100px", "height": "100px"},
+                ),
+                center_content=True,
+            ),
+            rx.container(
+                rx.vstack(
+                    rx.container(
+                        rx.input(placeholder="Username", on_blur=AuthState.set_username, mb=4),
+                        rx.input(
+                            type_="password",
+                            placeholder="Password",
+                            on_blur=AuthState.set_password,
+                            mb=4,
+                        ),
+                        rx.button(
+                            "Log in",
+                            on_click=AuthState.login,
+                            bg="blue.500",
+                            color="white",
+                            _hover={"bg": "blue.600"},
+                        ),
+                        center_content=True,
+                        align_items="left",
+                        bg="white",
+                        border="1px solid #eaeaea",
+                        p=4,
+                        max_width="400px",
+                        border_radius="lg",
+                    ),
+                    rx.text(
+                        "Don't have an account yet? ",
+                        rx.link("Sign up here.", href="/signup", color="blue.500"),
+                        color="gray.600",
+                    ),            
+                ),
+            ),
+            width='500px',
+            height='auto',
+            center_content=True,
+            borderRadius='20px',
+            boxShadow='9px 9px 50px #ceddf5',
+        ),
+        center_content=True,
+        # justifyContent='center',
+        maxWidth='auto',
+        maxHeight='auto',
+        height='100vh',
+        bg="url('aurora.jpg')",
+    )
+
